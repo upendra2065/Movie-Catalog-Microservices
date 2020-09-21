11f8721ee7525c8fe5aa5327e4cf6d93e8b39b84
Upendra Kumar <upendrakumariitian2017@gmail.com>
2020-09-22 01:59:42 +0530

    Use service discovery to call APIs

--- movie-catalog-service/src/main/java/com/domain/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/domain/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,12 +2,14 @@ package com.domain.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
+    @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
