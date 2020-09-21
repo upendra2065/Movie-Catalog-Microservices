b247cd6a6d45f5c2f3e1c04f667a35c6fa949bc0
Upendra Kumar <upendrakumariitian2017@gmail.com>
2020-09-21 19:12:27 +0530

    Added Movie Info API

---
+++ movie-info-service/src/main/java/com/domain/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,17 @@
+package com.domain.movieinfoservice.resources;
+
+
+import com.domain.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable String movieId){
+        return new Movie(movieId, "Test name");
+    }
+}
